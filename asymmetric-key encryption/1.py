b160ba03699a85bc806c93c05fb0a9225672938f
WooJJam <jaemin5548@naver.com>
2023-03-14 03:02:23 +0900

    비대칭키 암호화

--- asymmetric-key encryption/1.py
+++ asymmetric-key encryption/1.py
@@ -3,13 +3,8 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 def read_plaintext():
-    h = open('D:\\개쩌는 VS코드!\\Python\\Network Security and Blockchain\\asymmetric-key encryption\\src\\ex1.txt','r')
-    plaintext=""
-    while True:
-        line = h.readline()
-        if not line: 
-            break
-        plaintext += line
+    print("평문입력: ", end="")
+    plaintext = input()
     return plaintext
 
 def msg_encrypt(plaintext):
@@ -19,14 +14,14 @@ def msg_encrypt(plaintext):
     return enc_msg, key
 
 def key_encrypt(key):
-    h = open('D:\\개쩌는 VS코드!\\Python\\Network Security and Blockchain\\asymmetric-key encryption\\src\\public_key.pem','r')
+    h = open('D:\\Network Security and Blockchain\\asymmetric-key encryption\\src\\public_key.pem','r')
     public_key = RSA.importKey(h.read())
     encryptor = PKCS1_OAEP.new(public_key)
     enc_key = encryptor.encrypt(key)
     return enc_key
 
 def key_decrypt(enc_key):
-    h = open('D:\\개쩌는 VS코드!\\Python\\Network Security and Blockchain\\asymmetric-key encryption\\src\\private_key.pem','r')
+    h = open('D:\\Network Security and Blockchain\\asymmetric-key encryption\\src\\private_key.pem','r')
     private_key = RSA.import_key(h.read())
     encryptor = PKCS1_OAEP.new(private_key)
     aes_key = encryptor.decrypt(enc_key)
@@ -35,17 +30,19 @@ def key_decrypt(enc_key):
 
 def msg_decrypt(enc_msg, aes_key):
     dec_fernet = Fernet(aes_key) 
-    msg = dec_fernet.decrypt(enc_msg)
-    return msg
+    dec_msg = dec_fernet.decrypt(enc_msg)
+    return dec_msg
 
 
 if __name__ == '__main__':
     plaintext = read_plaintext()
     enc_msg, key = msg_encrypt(plaintext.encode())
+    print(f'암호화된 문자열: {enc_msg}')
     enc_key = key_encrypt(key)
+    print(f'암호화된 AES key: {enc_key}')
     aes_key = key_decrypt(enc_key)
-    msg = msg_decrypt(enc_msg, aes_key)
-    print(msg)
+    dec_msg = msg_decrypt(enc_msg, aes_key)
+    print(f'복호화된 문자열: {dec_msg}')
 
 
 
--- asymmetric-key encryption/2.py
+++ asymmetric-key encryption/2.py
@@ -3,67 +3,56 @@ import random
 import time
 import hashlib
 
-# SECP256K1 타원 곡선 관련 상수 정의
 p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
-a = 0x0000000000000000000000000000000000000000000000000000000000000000
-b = 0x0000000000000000000000000000000000000000000000000000000000000007
 Gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
 Gy = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+a = 0
+b = 7
 
-# Extended Euclidean Algorithm을 이용한 역원 계산 함수
-def modinv(a, n):
-    t, newt = 0, 1
-    r, newr = n, a
-
-    while newr != 0:
-        quotient = r // newr
-        t, newt = newt, t - quotient * newt
-        r, newr = newr, r - quotient * newr
-
-    if r > 1:
-        return None
-    if t < 0:
-        t += n
-
-    return t
-
-# 개인키 생성 함수
 def generate_private_key():
     while True:
-        # 강화된 randomness 적용
-        seed = os.urandom(16) + str(random.random()).encode() + str(time.time()).encode()
-        private_key = hashlib.sha256(seed).hexdigest()
-
-        # 생성된 개인키가 SECP256K1 타원 곡선의 p보다 작은지 확인
-        if int(private_key, 16) < p:
+        rand_str = os.urandom(32) + str(random.random()).encode() + str(time.time()).encode()
+        private_key = int(hashlib.sha256(rand_str).hexdigest(), 16)
+        
+        if private_key < p:
             return private_key
 
-# 공개키 생성 함수
-def generate_public_key(private_key):
-    # 개인키를 정수로 변환
-    d = int(private_key, 16)
+def extended_euclidian(a, b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        gcd, x, y = extended_euclidian(b, a % b)
+        q = a//b
+        return gcd, y, x - q * y
 
-    # Double-and-Add 알고리즘을 이용한 곱셈 연산
-    Qx, Qy = Gx, Gy
-    for i in range(256):
-        if d & (1 << i):
-            Qx, Qy = (Qx * 2 + Qy * 2 + Qx + a) % p, (Qx * 3 + Qy + b) % p
-        else:
-            Qx, Qy = (Qx * 2 + a) % p, (Qy * 2 + a) % p
+def double_and_add(key, P):
+    Q = None
+    for i in bin(key)[2:]:
+        Q = add_point(Q, Q)
+        if i == '1':
+            Q = add_point(Q, P)
+    return Q
 
-    # 곱셈의 역원 계산
-    inv = modinv(d, p)
+def add_point(P, Q):
+    if P is None:
+        return Q
+    if P[0] == Q[0]:
+        inverse = extended_euclidian(2 * P[1], p)[1]
+        lam = ((3 * P[0] **2 + a) * inverse) % p
+    else:
+        inverse = extended_euclidian(Q[0] - P[0], p)[1]
+        lam = ((Q[1] - P[1]) * inverse) % p
 
-    # 직선 방정식을 이용한 더하기 연산
-    R = ((Qx * inv ** 2) % p, ((-Qy) * inv ** 3) % p)
+    x3 = (lam * lam - P[0] - Q[0]) % p
+    y3 = (lam * (P[0] - x3) - P[1]) % p
 
-    return R
-
-# 개인키와 공개키 출력
-private_key = generate_private_key()
-public_key = generate_public_key(private_key)
-
-abc = modinv(6,13)
-print(abc)
-print("Private key:", private_key)
-print("Public key: (", hex(public_key[0]), ",", hex(public_key[1]), ")")
+    return x3 ,  y3
+        
+if __name__ == '__main__':
+    private_key =generate_private_key()
+    P = double_and_add(private_key, (Gx, Gy))
+    public_key = P[0], P[1]
+    print(f"개인키(16진수): {hex(private_key)}")
+    print(f"개인키(10진수): {private_key}")
+    print(f"공개키(16진수): ({hex(public_key[0])}), ({hex(public_key[1])})")
+    print(f"공개키(10진수): ({public_key[0]}), ({public_key[1]})")
